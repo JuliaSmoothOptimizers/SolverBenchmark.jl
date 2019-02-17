21e54fba99163dfaf9814f1b33719cf0e36dde39
Dominique Orban <dominique.orban@gmail.com>
2019-02-17 15:32:54 -0500

    multiple profiles + PkgBenchmark profiles

---
+++ examples/pkgbmark.jl
@@ -0,0 +1,24 @@
+# need to have done, e.g.,
+# results = PkgBenchmark.benchmarkpkg("Krylov", script="benchmark/cg_bmark.jl")
+
+# optional: customize plot appearance
+using Plots
+pyplot()  # recommended!
+using PlotThemes
+theme(:juno)
+
+common_plot_args = Dict{Symbol,Any}(
+  :linewidth => 2,
+  :alpha => .75,
+  :titlefontsize => 8,
+  :legendfontsize => 8,
+  :xtickfontsize => 6,
+  :ytickfontsize => 6,
+  :guidefontsize => 8,
+)
+Plots.default(; common_plot_args...)
+
+# process benchmark results and post gist
+p = profile_solvers(results)
+posted_gist = to_gist(results, svgfilename)
+println(posted_gist.html_url)
