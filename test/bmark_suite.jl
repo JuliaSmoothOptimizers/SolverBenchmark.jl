0e93f77d6609873ed052d598e5503ebc1cd1d11a
Dominique Orban <dominique.orban@gmail.com>
2020-07-13 23:39:37 -0400

    improve pkgbenchmark tools

--- test/bmark_suite.jl
+++ test/bmark_suite.jl
@@ -4,11 +4,11 @@ using BenchmarkTools
 
 # make up bogus benchmark suite
 SUITE = BenchmarkGroup()
-SUITE["fact"] = BenchmarkGroup()
+SUITE["lu"] = BenchmarkGroup()
+SUITE["qr"] = BenchmarkGroup()
 for _ = 1 : 10
     A = rand(10, 10)
     name = string(gensym())
-    SUITE["fact"][name] = BenchmarkGroup()
-    SUITE["fact"][name]["lu"] = @benchmarkable lu($A)
-    SUITE["fact"][name]["qr"] = @benchmarkable qr($A)
+    SUITE["lu"][name] = @benchmarkable lu($A)
+    SUITE["qr"][name] = @benchmarkable qr($A)
 end
