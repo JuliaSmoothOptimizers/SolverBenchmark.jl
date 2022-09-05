e793fd5e4b02049b3ebf626e6f8afbff1ddda70b
Monssaf Toukal <tmonssaf@gmail.com>
2022-09-05 13:21:41 -0400

    bmark solvers in parallel using addprocs

--- src/bmark_solvers.jl
+++ src/bmark_solvers.jl
@@ -15,14 +15,16 @@ Any keyword argument accepted by `solve_problems`
 #### Return value
 A Dict{Symbol, DataFrame} of statistics.
 """
-function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
+function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...;parallel_eval=false, kwargs...)
   stats = Dict{Symbol, DataFrame}()
-  if length(procs()) > 1
+  if parallel_eval && length(procs()) > 1
     @info "bmark solvers in parallel"
     future_stats = Dict{Symbol, Future}()
     @sync for (name, solver) in solvers
-      @debug "running" name solver
-      @async future_stats[name] = @spawnat :any solve_problems(solver, args...; kwargs...)
+      @async future_stats[name] = @spawnat :any begin
+        @info "worker $(myid()) running solver $(name)"
+        solve_problems(solver, args...; kwargs...)
+      end
     end
     @sync for (name, future) in future_stats
       @async stats[name] = fetch(future)
