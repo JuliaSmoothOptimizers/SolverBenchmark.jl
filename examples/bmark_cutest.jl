62347a662771d295d1641b3cdd178768c3ed8250
Dominique Orban <dominique.orban@gmail.com>
2024-05-28 17:45:21 -0400

    example benchmark scripts
    the scripts cover problems from
    
    1. CUTEst
    2. AMPL
    3. OptimizationProblems/ADNLPProblems
    4. OptimizationProblems/PureJuMP.

---
+++ examples/bmark_cutest.jl
@@ -0,0 +1,24 @@
+using JLD2, Plots
+using CUTEst, JSOSolvers, NLPModelsIpopt, SolverBenchmark
+
+# define problems
+# for CUTEst models, define list of models as an iterator to avoid materializing all models at once
+problem_names = CUTEst.select(max_con=0, min_var=50, max_var = 80, only_free_var=true)
+problems = (CUTEstModel(p) for p in problem_names)
+
+# define solvers
+solvers = Dict(
+  :trunk => nlp -> trunk(nlp, atol = 1.0e-4, rtol = 1.0e-5, max_time = 10.0, verbose = 0),
+  :ipopt => nlp -> ipopt(nlp, tol = 1.0e-5, max_cpu_time = 10.0, print_level = 0, sb = "no"),
+)
+
+# solve problems, but skip one of our choice
+to_skip = ["DMN37142LS"]
+stats = bmark_solvers(solvers, problems, skipif = prob -> prob.meta.name ∈ to_skip)
+
+# save DataFrame for later
+# see JLD2 documentation
+@save "stats_cutest.jld2" stats
+
+# plot time profile
+performance_profile(stats, df -> df.neval_obj)
---
+++ examples/bmark_opt_problems.jl
@@ -0,0 +1,24 @@
+using JLD2, Plots
+using ADNLPModels, JSOSolvers, NLPModelsIpopt, OptimizationProblems, OptimizationProblems.ADNLPProblems, SolverBenchmark
+
+# define problems
+probs = OptimizationProblems.meta
+problem_names = probs[(probs.ncon .== 0) .& .!probs.has_bounds .& (5 .<= probs.nvar .<= 100), :name]
+problems = (eval(Meta.parse(problem))() for problem ∈ problem_names)
+
+# define solvers
+solvers = Dict(
+  :trunk => nlp -> trunk(nlp, atol = 1.0e-4, rtol = 1.0e-5, max_time = 10.0, verbose = 0),
+  :ipopt => nlp -> ipopt(nlp, tol = 1.0e-5, max_cpu_time = 10.0, print_level = 0, sb = "no"),
+)
+
+# solve problems, but skip one of our choice
+to_skip = ["thurber"]
+stats = bmark_solvers(solvers, problems, skipif = prob -> prob.meta.name ∈ to_skip)
+
+# save DataFrame for later
+# see JLD2 documentation
+@save "stats_opt_problems.jld2" stats
+
+# plot time profile
+performance_profile(stats, df -> df.neval_obj)
