c1b89ee717343412c5b18a98f6f9341137fa6840
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2024-11-12 13:40:33 -0500

    updated based on the code review

--- src/bmark_solvers.jl
+++ src/bmark_solvers.jl
@@ -1,4 +1,4 @@
-export bmark_solvers, bmark_solvers_parallel
+export bmark_solvers
 
 using Base.Threads
 using DataFrames
@@ -6,7 +6,7 @@ using DataFrames
 """
     bmark_solvers(solvers :: Dict{Symbol,Any}, args...; kwargs...)
 
-Run a set of solvers on a set of problems.
+Run a set of solvers on a set of problems. If Threads.@threads is larger than 1, the solvers will be run in parallel.
 
 #### Arguments
 * `solvers`: a dictionary of solvers to which each problem should be passed
@@ -19,33 +19,6 @@ Any keyword argument accepted by `solve_problems`
 A Dict{Symbol, AbstractExecutionStats} of statistics.
 """
 function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
-  stats = Dict{Symbol, DataFrame}()
-  for (name, solver) in solvers
-    @info "running solver $name"
-    stats[name] = solve_problems(solver, name, args...; kwargs...)
-  end
-  return stats
-end
-
-
-"""
-    bmark_solvers_parallel(solvers :: Dict{Symbol, Any}, args...; kwargs...)
-
-Run a set of solvers on a set of problems in parallel.
-
-#### Arguments
-* `solvers`: a dictionary of solvers to which each problem should be passed.
-* `args...`: other positional arguments accepted by `solve_problems`, except for a solver name.
-
-#### Keyword arguments
-Any keyword argument accepted by `solve_problems`.
-
-#### Return value
-A `Dict{Symbol, DataFrame}` of execution statistics, with each solver's results stored under its corresponding key.
-"""
-
-
-function bmark_solvers_parallel(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
   stats = Dict{Symbol, DataFrame}()
 
   # Collect solvers in a vector so we can iterate them in parallel
@@ -61,6 +34,7 @@ function bmark_solvers_parallel(solvers::Dict{Symbol, <:Any}, args...; kwargs...
       result = solve_problems(solver, name, args...; kwargs...)
 
       # Update the shared stats dictionary safely
+      # This is a workaround for the lack of thread-safe dictionaries in Julia, so if two solver finish at same time, one of them will lock the stats first then the other one will wait until the lock is released to update the stats
       lock = ReentrantLock()  # Create a lock for thread-safe dictionary update
       lock(()-> stats[name] = result)
   end
