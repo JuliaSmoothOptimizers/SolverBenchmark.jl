693ad7aeb9b856261208ef3e6db577cb549efd3d
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2024-11-12 14:32:01 -0500

    Update bmark_solvers.jl

--- src/bmark_solvers.jl
+++ src/bmark_solvers.jl
@@ -1,9 +1,11 @@
-export bmark_solvers
 using Base.Threads
+
+export bmark_solvers
 """
     bmark_solvers(solvers :: Dict{Symbol,Any}, args...; kwargs...)
 
 Run a set of solvers on a set of problems. If Threads.@threads is larger than 1, the solvers will be run in parallel.
+Note that the @threads macro requires that the number of threads is set before the Julia process starts, using the -t or --threads command-line argument, or the JULIA_NUM_THREADS environment variable.
 
 #### Arguments
 * `solvers`: a dictionary of solvers to which each problem should be passed
@@ -15,16 +17,12 @@ Any keyword argument accepted by `solve_problems`
 #### Return value
 A Dict{Symbol, AbstractExecutionStats} of statistics.
 """
-function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
+
+function bmark_solvers_threaded(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
   stats = Dict{Symbol, DataFrame}()
-  solver_keys = collect(keys(solvers))
-  
-  @threads for i in 1:length(solver_keys)
-      name = solver_keys[i]
-      solver = solvers[name]
-      @info "running solver $name on thread $(threadid())"
+  @threads for (name, solver) in collect(pairs(solvers))
+      @info "running solver $name"
       stats[name] = solve_problems(solver, name, args...; kwargs...)
   end
-  
   return stats
 end
