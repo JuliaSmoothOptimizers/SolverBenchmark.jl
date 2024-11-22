119af1ac5eb325571b8a552cf7a6600ee00ea7f0
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2024-11-22 10:50:02 -0500

    Update bmark_solvers.jl

--- src/bmark_solvers.jl
+++ src/bmark_solvers.jl
@@ -22,23 +22,33 @@ Any keyword argument accepted by `solve_problems`
 A Dict{Symbol, AbstractExecutionStats} of statistics.
 """
 
-function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; threads_enable=false, kwargs...)
+function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; threads_enable = false, kwargs...)
   stats = Dict{Symbol, DataFrame}()
-  
+
   if threads_enable
-      @info "Running with multithreading enabled"
-      Threads.@threads for i in 1:length(solvers)
-          name = keys(solvers)[i]
-          solver = solvers[name]
-          @info "Running solver $name on thread $(Threads.threadid())"
-          stats[name] = solve_problems(solver, name, args...; kwargs...)
+    @info "Running with multithreading enabled"
+    key_array = collect(keys(solvers))  # Convert keys to an array for indexing
+    lock = Threads.Mutex()              # Create a mutex for thread-safe access to stats
+
+    Threads.@threads for i in eachindex(key_array)
+      name = key_array[i]
+      solver = solvers[name]
+      @info "Running solver $name on thread $(Threads.threadid())"
+      result = solve_problems(solver, name, args...; kwargs...)
+
+      Threads.lock(lock)              # Lock before modifying stats
+      try
+        stats[name] = result        # Thread-safe update
+      finally
+        Threads.unlock(lock)        # unlock the mutex
       end
+    end
   else
-      for (name, solver) in solvers
-          @info "Running solver $name"
-          stats[name] = solve_problems(solver, name, args...; kwargs...)
-      end
+    for (name, solver) in solvers
+      @info "Running solver $name"
+      stats[name] = solve_problems(solver, name, args...; kwargs...)
+    end
   end
-  
+
   return stats
 end
