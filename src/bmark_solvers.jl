4ff05e85aadd6a7e141f92cfb3f1fdfc81497361
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2024-11-25 10:37:10 -0500

    Update bmark_solvers.jl

--- src/bmark_solvers.jl
+++ src/bmark_solvers.jl
@@ -22,22 +22,24 @@ Any keyword argument accepted by `solve_problems`
 A Dict{Symbol, AbstractExecutionStats} of statistics.
 """
 function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
-  tasks = Vector{Task}(undef, length(solvers))
-  names = Vector{Symbol}(undef, length(solvers))
-  idx = 0
-  for (name, solver) in solvers
-      idx += 1
-      names[idx] = name
-      tasks[idx] = Threads.@spawn begin
-          @info "running solver $name"
-          solve_problems(solver, name, args...; kwargs...)
-      end
-  end
+    tasks = Vector{Task}(undef, length(solvers))
+    names = Vector{Symbol}(undef, length(solvers))
+    idx = 0
+    for (name, solver) in solvers
+        idx += 1
+        names[idx] = name
+        # Make a deep copy of the problems for this task so the race condition doesn't occur
+        local_args = deepcopy.(args)
+        tasks[idx] = Threads.@spawn begin
+            @info "running solver $name"
+            solve_problems(solver, name, local_args...; kwargs...)
+        end
+    end
 
-  stats = Dict{Symbol, DataFrame}()
-  for i in 1:length(tasks)
-      result = fetch(tasks[i])
-      stats[names[i]] = result
-  end
-  return stats
+    stats = Dict{Symbol, DataFrame}()
+    for i in 1:length(tasks)
+        result = fetch(tasks[i])
+        stats[names[i]] = result
+    end
+    return stats
 end
