d41e108c41e26ae893bc3ec4ad4fe07c35703661
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2025-01-16 10:11:21 -0500

    Update bmark_solvers.jl

--- src/bmark_solvers.jl
+++ src/bmark_solvers.jl
@@ -1,16 +1,9 @@
-using Base.Threads
-using DataFrames
-
 export bmark_solvers
 
 """
-    bmark_solvers(solvers :: Dict{Symbol, Any}, args...; kwargs...)
+    bmark_solvers(solvers :: Dict{Symbol,Any}, args...; kwargs...)
 
-Run a set of solvers on a set of problems. If `JULIA_NUM_THREADS` is set to a number greater than 1, 
-the solvers will be run in parallel using `Threads.@threads`.
-
-Note: The `@threads` macro requires that the number of threads be set before the Julia process starts, using the `-t` or `--threads`
-command-line argument, or the `JULIA_NUM_THREADS` environment variable.
+Run a set of solvers on a set of problems.
 
 #### Arguments
 * `solvers`: a dictionary of solvers to which each problem should be passed
@@ -36,7 +29,7 @@ function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
     end
 
   stats = Dict{Symbol, DataFrame}()
-  @threads for (name, solver) in collect(pairs(solvers))
+  for (name, solver) in solvers
     @info "running solver $name"
     stats[name] = solve_problems(solver, name, args...; kwargs...)
   end
