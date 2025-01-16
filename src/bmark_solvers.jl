8cb7da7313636ac349e700b05fbaa1eddaad917f
farhadrclass <31899325+farhadrclass@users.noreply.github.com>
2025-01-16 13:54:16 -0500

    Merge branch 'parallel'

--- src/bmark_solvers.jl
+++ src/bmark_solvers.jl
@@ -16,18 +16,6 @@ Any keyword argument accepted by `solve_problems`
 A Dict{Symbol, AbstractExecutionStats} of statistics.
 """
 function bmark_solvers(solvers::Dict{Symbol, <:Any}, args...; kwargs...)
-    tasks = Vector{Task}(undef, length(solvers))
-    names = Vector{Symbol}(undef, length(solvers))
-    idx = 0
-    for (name, solver) in solvers
-        idx += 1
-        names[idx] = name
-        tasks[idx] = Threads.@spawn begin
-            @info "running solver $name"
-            solve_problems(solver, name, args...; kwargs...)
-        end
-    end
-
   stats = Dict{Symbol, DataFrame}()
   for (name, solver) in solvers
     @info "running solver $name"
