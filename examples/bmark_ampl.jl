2281454c196b4cd0629c244504f5b60cc299b252
arnavk23 <kapoorarnav43@gmail.com>
2026-04-06 04:57:33 +0530

    Fix #172: Remove Percival from AMPL examples due to missing linear API support
    AmplNLReader does not support the linear API that Percival's SlackModel requires.
    Only Ipopt is now used for AMPL problems in the benchmark examples.

--- examples/bmark_ampl.jl
+++ examples/bmark_ampl.jl
@@ -1,13 +1,13 @@
 using JLD2, Plots
-using AmplNLReader, Percival, NLPModelsIpopt, SolverBenchmark
+using AmplNLReader, NLPModelsIpopt, SolverBenchmark
 
 # define problems
 problem_names = readdir(joinpath(@__DIR__, "ampl"))
 problems = (AmplModel(joinpath(@__DIR__, "ampl", p)) for p in problem_names)
 
 # define solvers
+# Note: Percival is excluded because AmplNLReader does not support the linear API required by SlackModel
 solvers = Dict(
-  :percival => nlp -> percival(nlp, atol = 1.0e-4, rtol = 1.0e-5, max_time = 10.0, verbose = 0),
   :ipopt => nlp -> ipopt(nlp, tol = 1.0e-5, max_cpu_time = 10.0, print_level = 0, sb = "no"),
 )
 
