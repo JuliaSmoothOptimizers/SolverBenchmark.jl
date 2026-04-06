87e5a39ef84a57e3b1534ad78fba560016374a63
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-04-06 20:35:47 +0530

    Update bmark_ampl.jl

--- examples/bmark_ampl.jl
+++ examples/bmark_ampl.jl
@@ -6,9 +6,20 @@ problem_names = readdir(joinpath(@__DIR__, "ampl"))
 problems = (AmplModel(joinpath(@__DIR__, "ampl", p)) for p in problem_names)
 
 # define solvers
-# Note: Percival is excluded because AmplNLReader does not support the linear API required by SlackModel
 solvers = Dict(
   :ipopt => nlp -> ipopt(nlp, tol = 1.0e-5, max_cpu_time = 10.0, print_level = 0, sb = "no"),
+  using JLD2, Plots
+using AmplNLReader, NLPModelsIpopt, SolverBenchmark
+
+# define problems
+problem_names = readdir(joinpath(@__DIR__, "ampl"))
+problems = (AmplModel(joinpath(@__DIR__, "ampl", p)) for p in problem_names)
+
+# define solvers
+solvers = Dict(
+  :ipopt => nlp -> ipopt(nlp, tol = 1.0e-5, max_cpu_time = 10.0, print_level = 0, sb = "no"),
+  :ipopt_lm => nlp -> ipopt(nlp, tol = 1.0e-5, max_cpu_time = 10.0, print_level = 0, sb = "no",
+                            hessian_approximation = "limited-memory"),
 )
 
 # solve problems, but skip one of our choice
